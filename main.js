99b144b0b5593e464a3a8a844a34d8abd05a4a97
Mason Walker <46076583+cubesniper24@users.noreply.github.com>
2021-05-14 10:09:17 -0400

    Update main.js

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 var canvas = document.getElementById("myCanvas");
 var ctx = canvas.getContext("2d");
 var player = {x: 240, y: 160, r: 10};
+var plat1 = {x: 380, y: 260, w: 100, h: 30};
 var testText = document.getElementById("text");
 setInterval(update, 10);
 
@@ -8,16 +9,25 @@ function update()
 {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
     drawPlayer(player.x, player.y, player.r);
+    drawRect(plat1.x, plat1.y, plat1.w, plat1.h);
     testText.innerHTML = rightArrowDown;
     movement();
 }
 //drawing gameObjects
 
+function drawRect(rectx, recty, rectw, recth)
+{
+    ctx.beginPath();
+    ctx.rect(rectx, recty, rectw, recth);
+    ctx.fillStyle = "blue";
+    ctx.fill();
+    ctx.closePath();
+}
 function drawPlayer(playerx, playery, radius)
 {
     ctx.beginPath();
     ctx.arc(playerx, playery, radius, 0, 2 * Math.PI, false);
-    ctx.fillStyle = "blue";
+    ctx.fillStyle = "green";
     ctx.fill();
     ctx.closePath();
 }
