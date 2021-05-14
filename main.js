000643a01f5d8811f074846bcff4c07df1b67894
Mason Walker <46076583+cubesniper24@users.noreply.github.com>
2021-05-14 08:23:24 -0400

    Update main.js

--- main.js
+++ main.js
@@ -1,3 +1,18 @@
+var canvas = document.getElementById("myCanvas");
+var ctx = canvas.getContext("2d");
+setInterval(update, 10);
 
-    
+function update()
+{
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    drawPlayer(canvas.width/2, canvas.height/2, 10);
+}
+
+function drawPlayer(playerx, playery, radius)
+{
+    ctx.beginPath();
+    ctx.arc(playerx, playery, radius, 0, 2*Math.PI);
+    ctx.fillStyle("blue");
+    ctx.endPath();
+}
 
