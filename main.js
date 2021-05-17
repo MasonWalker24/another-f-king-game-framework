7f28eb8b0a2c42dfde364dd96c7896bc9dda2e06
Mason Walker <46076583+cubesniper24@users.noreply.github.com>
2021-05-17 12:36:57 -0400

    Update main.js

--- main.js
+++ main.js
@@ -13,7 +13,7 @@ function update()
     //plat1
     drawRect(plat1.x, plat1.y, plat1.w, plat1.h);
     //plat2
-    testText.innerHTML = player.vx;
+    testText.innerHTML = veloXCounter;
     movement();
     player.vy = 0;
     gravity();
