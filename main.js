a5bca7dce217a86ef7b32c632a2fe6ce06968f82
Mason Walker <46076583+cubesniper24@users.noreply.github.com>
2021-05-17 09:56:37 -0400

    Update main.js

--- main.js
+++ main.js
@@ -79,7 +79,7 @@ function movement()
     {
         if(velocityX < 2)
             {
-                velocityX = velocityX + 0.2;
+                velocityX = velocityX + 0.1;
             }
     }
     if(leftArrowDown)
@@ -88,7 +88,7 @@ function movement()
         {
             if(velocityX > -2)
             {
-                velocityX = velocityX - 0.2;
+                velocityX = velocityX - 0.1;
             }
         }
     }
