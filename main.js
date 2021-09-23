926485b7f580489130c1cc8d41fc257c0f70690b
Mason Walker <46076583+cubesniper24@users.noreply.github.com>
2021-09-23 09:57:55 -0400

    Update main.js

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 var canvas = document.getElementById("myCanvas");
 var ctx = canvas.getContext("2d");
 var player = {x: 240, y: 160, r: 15, vx: 0, vy: 0};
-var plat1 = {x: 380, y: 260, w: 100, h: 30, function test() {alert("test");}};
+  var plat1 = {x: 380, y: 260, w: 100, h: 30, test: function() {alert("test");}};
 var testText = document.getElementById("text");
 setInterval(update, 10);
 setInterval(hitDetection, 10);
