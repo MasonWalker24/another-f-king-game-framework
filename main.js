5933a64a42ebda6d264b1db1940deb8b12a3708e
Mason Walker <46076583+cubesniper24@users.noreply.github.com>
2021-09-23 12:52:57 -0400

    Update main.js

--- main.js
+++ main.js
@@ -3,8 +3,8 @@ var ctx = canvas.getContext("2d");
 var player = {x: 240, y: 160, r: 15, vx: 0, vy: 0};
 var plat1 = {x: 380, y: 260, w: 100, h: 30};
 var testText = document.getElementById("text");
-setInterval(update, 10);
-setInterval(hitDetection, 10);
+setInterval(update, 1);
+setInterval(hitDetection, 1);
 function update() {
 if(input.keyPressed == KeyCode.Space)
 {
