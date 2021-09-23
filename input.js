ab26ba815851a375889178409e71e83eabb8c107
Mason Walker <46076583+cubesniper24@users.noreply.github.com>
2021-09-23 12:02:54 -0400

    Update input.js

--- input.js
+++ input.js
@@ -1,3 +1,7 @@
-var keycode = {Space: 32};
-var input = {getKeyDown: function(int key) {}};
-document.addEventListener("keydown", input.getKeyDown, "false");
+var KeyCode = {Space: 32};
+var input = {keyPressed: null};
+document.addEventListener("keydown", setKey, "false");
+function setKey(event)
+{
+  input.keyPressed = event.which;
+}
